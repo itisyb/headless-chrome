afaae94835cd4660001a6ed8fe217f22ab4c156c
itisyb <42640264+itisyb@users.noreply.github.com>
2022-02-15 12:36:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,17 +20,49 @@ app.get('/', function(req, res) {
         console.log('Screenshotting: ' + urlToScreenshot);
         (async() => {
             const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: [
+            '--disable-web-security',
+            '--disable-features=IsolateOrigins,site-per-process',
+            '--allow-running-insecure-content',
+            '--disable-blink-features=AutomationControlled',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--mute-audio',
+            '--no-zygote',
+            '--no-xshm',
+            '--window-size=1920,1080',
+            '--no-first-run',
+            '--no-default-browser-check',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--enable-webgl',
+            '--ignore-certificate-errors',
+            '--lang=en-US,en;q=0.9',
+            '--password-store=basic',
+            '--disable-gpu-sandbox',
+            '--disable-software-rasterizer',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-renderer-backgrounding',
+            '--disable-infobars',
+            '--disable-breakpad',
+            '--disable-canvas-aa',
+            '--disable-2d-canvas-clip-aa',
+            '--disable-gl-drawing-for-tests',
+            '--enable-low-end-device-mode',
+            '--disable-extensions-except=./plugin',
+            '--load-extension=./plugin'
+        ]
             });
 
             const page = await browser.newPage();
             await page.goto(urlToScreenshot);
-            await page.screenshot().then(function(buffer) {
-                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
-                res.setHeader('Content-Type', 'image/png');
-                res.send(buffer)
-            });
-
+            await page.setViewport({ width: 1920, height: 1009 });
+            await page.waitForSelector('.go')
+            await page.click('.go')
+            await page.waitForTimeout(3000);
+            await page.waitForSelector('.response');
+            res.send('yo')
             await browser.close();
         })();
     } else {
